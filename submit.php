37326f52b49bc95be6b8f9b5748163a71bda35d2
Rutviz Vyas <Rutviz Vyas>
2016-08-20 00:06:04 +0530

    version 1.3

--- submit.php
+++ submit.php
@@ -14,18 +14,25 @@
 		while($res=mysqli_fetch_assoc($query))
 		{
 			$show=$res['inp'];
+			$img=strrev($show);
+			$mg=explode('.',$img);
+			$m=$mg[0];
 			$arr=explode('.',$show);
 			$bbb=explode(':',$show);
 			$i=$arr[0];
 			$j=$bbb[0];
-				if($j=="http" || $j=="https")
+				if($m=="gpj" || $m == "gepj" || $m=="gnp")
+				{
+					?><div id="card2" z-default='20'><img src="<?php echo $show ?>" width="200px" height="200px"></img><br><a href="<?php echo $show ?>"><?php echo $show ?></a> </div> <br>  <?php 
+				}
+				else if($j=="http" || $j=="https")
 				{		
 					?>
 					<div id="card2" z-default='20'><iframe src="<?php echo $show ?>"></iframe><br><a href="<?php echo $show ?>"><?php echo $show ?></a> </div> <br>  <?php 
 				}
 				else if($i=="www")
 				{
-					?> <div id="card2" z-default='20'><iframe src="<?php echo $show ?>"></iframe><br><a href="<?php echo "http://".$show ?>"><?php echo $show ?></a> </div> <br>  <?php
+					?> <div id="card2" z-default='20'><iframe src="<?php echo "https://".$show ?>"></iframe><br><a href="<?php echo "http://".$show ?>"><?php echo $show ?></a> </div> <br>  <?php
 				}
 				else{
 				?> <div id="card2" z-default='20'> <?php echo $res['inp']; ?> </div> <br>  <?php
