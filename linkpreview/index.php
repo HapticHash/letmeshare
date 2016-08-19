d036d728f0117a0b33487ec9b407a0c35cef1bf6
Harshit Kumar Singh <harshitks2203@gmail.com>
2016-08-19 22:58:06 +0530

    ver 1.1

---
+++ linkpreview/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>  
+	<title>Let's Share</title>
+	
+    <link rel="stylesheet" type="text/css" href="css/style.css" >
+
+</head>
+
+<body>
+
+	This live preview for <a href="http://en.wikipedia.org/">Wikipedia</a><div class="box"><iframe src="http://en.wikipedia.org/" width = "500px" height = "500px"></iframe></div> remains open on mouseover.
+    
+
+</body>
+</html>
+
+
+
+
+
+
+ 
